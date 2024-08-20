e22f238a8d45c94c24cc7ca336c6c0f4567a0409
lucasstqa <lucas.leis0472@gmail.com>
2024-08-20 18:48:11 -0300

    ejercicio C finalizado

--- js/main.js
+++ js/main.js
@@ -44,10 +44,26 @@ for(var j=1 ; j<=cantidadDeGatos ; j++){
     console.log("Gato #" + j + ":🐈" + pasos)
 }
 
+console.log("---------Ejercicio C---------")
 
+/* Ejercicio c */
 
+var cantidadDeGatos = 10;
+var cantidadDePasos = 5;
 
+for(var l=1 ; l<=cantidadDeGatos ; l++){
 
+    var pasos = ""
 
+    for(var z=0 ; z<=cantidadDePasos ; z++){
+        pasos += "🐾"
+    }
 
+    if(l % 2 ==0){ 
+        console.log("Gato #" + l + ":🐈" + "⬛" + pasos)
+    }
 
+    else{
+        console.log("Gato #" + l + ":🐈" + pasos)
+    }
+}
